bd17a4be6e40f4dccebe42b85162b5de604f2248
LuanCavalcanti07 <Luancavalcanti08@gmail.com>
2023-12-04 13:20:41 -0300

    Adição da tela
    Falta juntar as 2 mains, como não consigo testar aqui preferi deixar o conteúdo em arquivos separados.

---
+++ Application/app/src/main/java/com/example/networkanalyzer/ResultActivity.java
@@ -0,0 +1,32 @@
+package com.example.tela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+
+
+        Button btnBackToMain = findViewById(R.id.bt_finish);
+        btnBackToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Criar uma Intent para ir para a MainActivity
+                Intent mainIntent = new Intent(ResultActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+
+                // Encerrar a ResultActivity
+                finish();
+            }
+        });
+    }
+}
